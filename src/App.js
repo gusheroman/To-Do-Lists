ee1359a8f4dc54b4465c1650fae06b55a0fc5bd7
gusheroman1 <tanadon.gus@mail.kmutt.ac.th>
2022-08-19 23:59:44 +0700

    Update update and create form

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Paper from "@material-ui/core/Paper";
 import DeleteIcon from "@material-ui/icons/Delete";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import CreateIcon from "@material-ui/icons/Create";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
 const useStyles = makeStyles({
   container: {
-    backgroundColor: "black",
+    backgroundColor: "#100720",
     color: "white",
     minHeight: "100vh",
     display: "flex",
@@ -18,7 +20,7 @@ const useStyles = makeStyles({
     alignItems: "center",
   },
   taskBackGround: {
-    backgroundColor: "blue",
+    backgroundColor: "#31087B",
     color: "white",
     minHeight: "50px",
     display: "flex",
@@ -38,13 +40,37 @@ const useStyles = makeStyles({
     textDecoration: "line-through",
     opacity: 0.3,
   },
-  icon: {
+  iconContainer: {
     position: "absolute",
     top: "50%",
     transform: " translateY(-50%)",
     right: "15px",
     width: "auto",
     display: "inline-block",
+    cursor: "pointer",
+  },
+  icon: {
+    cursor: "pointer",
+    padding: "0px 4px",
+    "&:hover": {
+      color: "#FFC23C",
+    },
+  },
+  input: {
+    "& .MuiFilledInput-input": {
+      backgroundColor: "white",
+      borderRadius: "8px",
+      padding: "16px",
+      width: "400px",
+    },
+    marginRight: "24px",
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  formControl: {
+    marginBottom: "16px",
   },
 });
 
@@ -58,28 +84,54 @@ function App() {
     <div className={classes.container}>
       <div className={classes.body}>
         <h2>To Do List</h2>
+        <div className={classes.formContainer}>
+          <div className={classes.formControl}>
+            <TextField
+              classes={{ root: classes.input }}
+              InputProps={{ disableUnderline: true }}
+              variant="filled"
+            />
+            <Button variant="contained" color="primary">
+              Update
+            </Button>
+          </div>
+          <div className={classes.formControl}>
+            <TextField
+              classes={{ root: classes.input }}
+              InputProps={{ disableUnderline: true }}
+              variant="filled"
+            />
+            <Button variant="contained" color="Secondary">
+              Add Task
+            </Button>
+          </div>
+        </div>
         <Paper className={classes.cardContainer}>
           <p>{toDo && toDo.length ? "" : "No Tasks..."}</p>
-          {toDo.map((task, index) => (
-            <React.Fragment key={task.id}>
-              <Paper className={classes.taskBackGround}>
-                <div className={task.status ? classes.doneTask : ""}>
-                  <span className={classes.taskTitle}>{task.title}</span>
-                </div>
-                <div className={classes.icon}>
-                  <span>
-                    <DeleteIcon />
-                  </span>
-                  <span>
-                    <CreateIcon />
-                  </span>
-                  <span>
-                    <CheckCircleIcon />
-                  </span>
-                </div>
-              </Paper>
-            </React.Fragment>
-          ))}
+          {toDo &&
+            toDo.map((task) => (
+              <React.Fragment key={task.id}>
+                <Paper className={classes.taskBackGround}>
+                  <div className={task.status ? classes.doneTask : ""}>
+                    <span className={classes.taskTitle}>{task.title}</span>
+                  </div>
+                  <div className={classes.iconContainer}>
+                    <span>
+                      <DeleteIcon className={classes.icon} fontSize="small" />
+                    </span>
+                    <span>
+                      <CreateIcon className={classes.icon} fontSize="small" />
+                    </span>
+                    <span>
+                      <CheckCircleIcon
+                        className={classes.icon}
+                        fontSize="small"
+                      />
+                    </span>
+                  </div>
+                </Paper>
+              </React.Fragment>
+            ))}
         </Paper>
       </div>
     </div>
